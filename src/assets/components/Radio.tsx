826ff6d074af0860c1fb4385e6e94e54a10734e3
dl-little <little.douglas22@gmail.com>
2024-08-12 17:30:41 -0400

    Accessible radio group

--- src/assets/components/Radio.tsx
+++ src/assets/components/Radio.tsx
@@ -1,25 +1,17 @@
-const Radio = () => {
+const Radio: React.FC<TRadio> = (props) => {
+  const { id, label, handleClick, activeIndex, index, fClass } = props;
+
   return (
-    <>
-      <h3 id="group_label_1">Pizza Crust</h3>
-      <ul
-        className="radiogroup-activedescendant"
-        role="radiogroup"
-        aria-labelledby="group_label_1"
-        aria-activedescendant="rb11"
-        tabIndex={0}
-      >
-        <li id="rb11" role="radio" aria-checked="false">
-          Regular crust
-        </li>
-        <li id="rb12" role="radio" aria-checked="false">
-          Deep dish
-        </li>
-        <li id="rb13" role="radio" aria-checked="false">
-          Thin crust
-        </li>
-      </ul>
-    </>
+    <li
+      id={id}
+      onClick={handleClick}
+      role="radio"
+      aria-checked={activeIndex === index}
+      data-index={`${index}`}
+      className={fClass}
+    >
+      {label}
+    </li>
   );
 };
 
---
+++ src/assets/components/RadioGroup.tsx
@@ -0,0 +1,120 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Children,
+  cloneElement,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  useContext,
+} from 'react';
+import { FormContext } from '../contexts/FormContextProvider';
+
+const RadioGroup: React.FC<IRadioGroup> = (props) => {
+  const { id, label, children } = props;
+  /* @ts-expect-error: TODO: Provide default in definition of context */
+  const { activeDescendant, setActiveDescendant } = useContext(FormContext);
+  const groupRef = useRef<HTMLUListElement | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [listItems, setListItems] = useState<ReactElement[]>(children);
+  const labelId = `${id}_label`;
+
+  const handleKeydown = (event: KeyboardEvent) => {
+    let flag = false;
+    let offset = 0;
+
+    switch (event.key) {
+      case ' ':
+        flag = true;
+        break;
+
+      case 'Up':
+      case 'ArrowUp':
+      case 'Left':
+      case 'ArrowLeft':
+        offset = -1;
+        flag = true;
+        break;
+
+      case 'Down':
+      case 'ArrowDown':
+      case 'Right':
+      case 'ArrowRight':
+        offset = 1;
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+      setActiveIndex((activeIndex) => {
+        return (activeIndex + offset + listItems.length) % listItems.length;
+      });
+    }
+  };
+
+  useEffect(() => {
+    const group = groupRef.current;
+
+    if (!group) {
+      return;
+    }
+
+    group.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      group.removeEventListener('keydown', handleKeydown);
+    };
+  }, []);
+
+  useEffect(() => {
+    const active = listItems.find((item) => {
+      return item?.props.index === activeIndex;
+    });
+
+    setActiveDescendant(active?.props.id);
+  }, [listItems]);
+
+  useEffect(() => {
+    setListItems(renderChildren());
+  }, [activeIndex]);
+
+  const renderChildren = useCallback(() => {
+    return Children.map(children, (child, index) => {
+      return cloneElement(child, {
+        handleClick: (e: Event) => {
+          // @ts-expect-error: TODO: find the right event type
+          setActiveIndex(Number(e.target?.dataset?.index));
+        },
+        activeIndex: activeIndex,
+        index: index,
+        ...(activeIndex === index && {
+          fClass: 'focus',
+        }),
+      });
+    });
+  }, [activeIndex]);
+
+  return (
+    <>
+      <h2 id={labelId}>{label}</h2>
+      <ul
+        className="radiogroup-activedescendant"
+        role="radiogroup"
+        aria-labelledby={labelId}
+        aria-activedescendant={activeDescendant}
+        tabIndex={0}
+        ref={groupRef}
+      >
+        {listItems}
+      </ul>
+    </>
+  );
+};
+
+export default RadioGroup;
