b6328e8a79cab8c262fd9e6e212d910f68701738
dl-little <little.douglas22@gmail.com>
2024-08-12 17:31:16 -0400

    Broad strokes styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { FormEventHandler } from 'react';
-import Radio from './assets/components/radio';
+import Form from './assets/components/Form';
+import RadioGroup from './assets/components/RadioGroup';
+import Radio from './assets/components/Radio';
+import WordInput from './assets/components/WordInput';
+import { FormContextProvider } from './assets/contexts/FormContextProvider';
 
 function App() {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-  };
-
   return (
     <main className="p-4 h-full flex flex-col justify-center items-center [&>*:not(:first-child)]:mt-4">
       <h1>Dictionary</h1>
-      <form
-        className="max-w-[768px] w-full [&>*:not(:first-child)]:mt-4"
-        onSubmit={handleSubmit}
-      >
-        <Radio />
-      </form>
+      <FormContextProvider>
+        <Form>
+          <RadioGroup id="query" label="Type of Query">
+            <Radio id="synonym" label="Synonym of:" />
+            <Radio id="definition" label="Definition of:" />
+            <Radio id="antonym" label="Antonym of:" />
+          </RadioGroup>
+          <WordInput />
+        </Form>
+      </FormContextProvider>
     </main>
   );
 }
