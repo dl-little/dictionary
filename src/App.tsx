62fcb395bd615fbbc9275bb5dde513a369c15172
dl-little <little.douglas22@gmail.com>
2024-08-14 15:51:07 -0400

    Prevent scroll from moving content

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { FormContextProvider } from './contexts/FormContextProvider';
 
 function App() {
   return (
-    <main className="p-4 lg:p-8 h-full flex flex-col justify-start items-start [&>*:not(:first-child)]:mt-4 gap-5">
+    <main className="p-4 lg:px-[10%] h-full flex flex-col justify-start items-start [&>*:not(:first-child)]:mt-4 gap-5">
       <h1>Dictionary</h1>
       <FormContextProvider>
         <section className="w-full flex justify-between gap-5 flex-wrap">
