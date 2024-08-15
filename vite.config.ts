4061ff45e634d76ff2aecdc2b7f2eb6605761e7e
dl-little <little.douglas22@gmail.com>
2024-08-15 12:22:28 -0400

    Fix base in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dictionary/',
   // @ts-expect-error: ignore in config.
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/tests/setup.js',
-    base: '/dictionary/',
   },
 });
